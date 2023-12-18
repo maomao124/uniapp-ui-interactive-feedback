d5a1d17904befa88810a4ae0f8bd608b2b99afd6
mao <1296193245@qq.com>
2023-12-18 09:11:59 +0800

    uniapp-ui-interactive-feedback

---
+++ pages/index/test7/test7.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<button type="primary" @click="loading1">showLoading</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			loading1()
+			{
+				uni.showLoading({
+					title:'加载中，请稍后...'
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 10px;
+}
+</style>
---
+++ pages/index/test8/test8.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<button type="primary" @click="loading1">showLoading</button>
+		<button type="primary" @click="hide">hideLoading</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			loading1()
+			{
+				uni.showLoading({
+					title:'加载中，请稍后...'
+				})
+			},
+			hide()
+			{
+				uni.hideLoading();
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 10px;
+}
+</style>
